b3014b0747d3d7b7fb73fa7c7de33ddbe301e4fb
Kacper Sleziak <kacpersleziak00@gmail.com>
2022-06-05 19:42:51 +0200

    commit

---
+++ kredek/src/SomeStuff.js
@@ -0,0 +1,13 @@
+function Stuff({chair, table}){
+
+    return(
+        <div>
+            Here is some random stuff:
+            <br/>
+            <span>-{chair}</span>
+            <br/>
+        </div>
+    )
+}
+
+export default Stuff;
